a16f4501e7daf7daa9bac1a897ae692c1108cf89
Pcfm17 <paulocesarfmotta17@gmail.com>
2025-04-16 16:56:13 -0300

    finalizando

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include "projeto.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <locale.h>
-
+#include <math.h>
 #define NOME_ARQUIVO "clientes.txt"
 #define MAX_LINE_SIZE 256 // Tamanho máximo de uma linha no arquivo
 
@@ -28,20 +25,44 @@ int main() {
 
     printf("Por favor, faça o login para continuar.\n\n");
 
-    printf("Escolha uma das opções de login:\n\n");
-
-    // As opções de login fixas foram removidas para usar os dados carregados do arquivo.
-
-    if (login(&lista_clientes) != 0) {
-        printf("Programa encerrado.\n");
+    int pergunta;
+    printf("Você possui um login? [1. Sim e 2.Não] digite um dos números:\n");
+    if (scanf("%d", &pergunta) != 1) {
+        printf("Entrada inválida.\n");
         return 1;
+    }
+    while (getchar() != '\n'); // Limpar o buffer do teclado
+
+    switch(pergunta){
+        case 1:
+            printf("LOGIN:\n");
+            if (login(&lista_clientes) != 0) {
+                printf("Falha no login ou programa encerrado.\n");
+                return 1;
+            }
+            break;
+        case 2:
+            criarCliente(&lista_clientes);
+            if (login(&lista_clientes) != 0) {
+                printf("Falha no login ou programa encerrado.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Opção inválida.\n");
+            return 1;
     }
 
     int escolha;
     do {
         exibirMenu();
         printf("Escolha uma opção: ");
-        scanf("%d", &escolha);
+        if (scanf("%d", &escolha) != 1) {
+            printf("Entrada inválida.\n");
+            while (getchar() != '\n'); // Limpar o buffer do teclado
+            continue;
+        }
+        while (getchar() != '\n'); // Limpar o buffer do teclado
 
         switch (escolha) {
             case 1:
@@ -63,10 +84,10 @@ int main() {
                 sacar(&lista_clientes);
                 break;
             case 7:
-                comprarCripto(&lista_clientes);
+                comprarCripto(&lista_clientes, bitcoin_price, ethereum_price, ripple_price);
                 break;
             case 8:
-                venderCripto(&lista_clientes);
+                venderCripto(&lista_clientes, bitcoin_price, ethereum_price, ripple_price);
                 break;
             case 9:
                 atualizarCotacao(&bitcoin_price, &ethereum_price, &ripple_price);
